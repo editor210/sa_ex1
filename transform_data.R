1fa1e4e22dc7c0c39a3fec4eee796af6f4011a52
editor210 <edimilson.torres@gmail.com>
2023-10-19 17:26:56 +0100

    Added comments to the code

--- transform_data.R
+++ transform_data.R
@@ -4,13 +4,21 @@ library(tidyverse)
 data <- AirPassengers
 
 data_transform <- function(data, by_year = FALSE){
+
+                                  #transforming the data variable into a dataframe
+                                  #and the values as integers
                                   data <- as.data.frame(as.integer(data))
+                                  
+                                  #Assigning the name of the column
                                   colnames(data) <- "Passengers"
+                                  #Including the Years and Months columns
                                   data <- data %>% mutate(Year = rep(seq(1949,1960), each = 12),
                                                           Month = rep(c("Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"), 12))
-                                  #Rearranging the columns
+                                  
+                                  #Rearranging the columns to make it more readable
                                   data <- data[c("Year", "Month", "Passengers")]
                                   
+                                  #Including the change of the second argument
                                   if (by_year == TRUE){
                                     data <- data %>% group_by(Year) %>% summarise(Passengers = sum(Passengers))
                                   }
