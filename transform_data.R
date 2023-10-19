7660a96714cc157cae7cd298fff357ac9a94e2cb
editor210 <edimilson.torres@gmail.com>
2023-10-19 17:17:00 +0100

    second argument added to group by Year

--- transform_data.R
+++ transform_data.R
@@ -3,13 +3,19 @@ library(tidyverse)
 
 data <- AirPassengers
 
-data_transform <- function(data){
+data_transform <- function(data, by_year = FALSE){
                                   data <- as.data.frame(as.integer(data))
                                   colnames(data) <- "Passengers"
                                   data <- data %>% mutate(Year = rep(seq(1949,1960), each = 12),
                                                           Month = rep(c("Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"), 12))
                                   #Rearranging the columns
                                   data <- data[c("Year", "Month", "Passengers")]
+                                  
+                                  if (by_year == TRUE){
+                                    data <- data %>% group_by(Year) %>% summarise(Passengers = sum(Passengers))
+                                  }
   
   return(data)
 }
+
+data2 <- data_transform(data, by_year = TRUE)
