85756fb5d95a49a41ae9d2b87211678a0db94ff4
editor210 <edimilson.torres@gmail.com>
2023-10-19 16:30:09 +0100

    Transform the time series into a tidy DataFrame

---
+++ transform_data.R
@@ -0,0 +1,15 @@
+#get the AirPassengers dataset from python and transform the data into a df
+library(tidyverse)
+
+data <- AirPassengers
+
+data_transform <- function(data){
+                                  data <- as.data.frame(data)
+                                  colnames(data) <- "Passengers"
+                                  data <- data %>% mutate(Year = rep(seq(1949,1960), each = 12),
+                                                          Month = rep(c("Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"), 12))
+                                  #Rearranging the columns
+                                  data <- data[c("Year", "Month", "Passengers")]
+  
+  return(data)
+}
